00f0e8be7acdbe8ca8872a435db62464df96e062
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-10 21:38:31 +0100

    New apps: Merging content from a few files to one

--- merging_files_to_one.py
+++ merging_files_to_one.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import csv
-from pathlib import Path
 
+
+from pathlib import Path
+# To create the list of filepathes
 filepathes = Path("files").glob("*.txt")
 
+# Create new file
 with open("new_file.txt", "w") as outfile:
+    # Iterate for each file in the list of filepathes
     for item in filepathes:
+        # Open each file and extract data
         with open(item, "r") as infile:
             content = infile.read()
             outfile.write(content)
