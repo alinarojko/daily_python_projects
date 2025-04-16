5d3928c387b18f7054fb9c2e986598efce1cf7e2
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-16 20:08:47 +0100

    New apps:Motion Camera with time stampt

---
+++ read_image.py
@@ -0,0 +1,12 @@
+import cv2
+
+array = cv2.imread("image.png")
+# To show size of image
+print(array.shape)
+
+#To produce an image from the array of pixels- colors
+import numpy
+import cv2
+
+a = numpy.array()
+
